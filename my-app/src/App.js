196409505ddeb60dd22bd0ee787807fa1dcb0538
marcus.oliveira <marcus.oliveira@WD19>
2023-03-20 09:53:00 -0400

    Creação da lista de tarefas

---
+++ my-app/src/App.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import './App.css'
+
+var arr = new Array();
+
+function App() {  
+  const [task, SetTask] = useState([])
+
+  return (
+    
+    <div className='App'>      
+      <div>
+        <label htmlFor="name">Task</label>
+        <br/>
+        <input type="text" id="name" />
+      </div>
+
+      <div>
+        <label htmlFor="date">Date</label>
+        <br/>
+        <input type="date" id="date" />
+      </div>
+
+      <div>
+        <input type="button" id="btn" value={"Enviar"}  onClick={ ()=> {
+
+          var nameValue = document.querySelector("#name").value
+          var dateValue = document.querySelector("#date").value
+
+          dateValue = dateValue.replace(/-/g, "/")
+
+          arr.push({id: arr.length, name: nameValue, data: dateValue});
+          SetTask(new Array(arr));
+        }} />
+      </div>
+      
+      <hr />
+
+      <ul>
+        {(task.length > 0 ) ? task[0].map((t)=>(
+          <li key={t.id}>
+              <p>
+                {t.name}
+              </p>
+              -
+              <p>
+                {t.data}
+              </p>
+          </li>
+        )) : (
+          <li>
+            <p>
+              Vazio
+            </p>
+          </li>
+          )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
